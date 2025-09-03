3ab6ab3ce91195e1a357af5deff16337b4315d3b
Nateheff <nateheffgk05@gmail.com>
2025-09-03 14:30:03 -0700

    Added Stage 1 memory and docs

--- testing.py
+++ testing.py
@@ -79,7 +79,14 @@ class PFC(nn.Module):
         #Delegate generation to the base model
         return self.base_model.generate(*args, **kwargs)
 
+"""
+The Entorhinal Layer
 
+The Entorhinal Layer is a dense layer receiving the query/queue from the LLM that projects it
+into a space that is understood and used by the AH. This layer doesn't need to be complex or fancy,
+just a single nonlinear projection. This layer is preparing the query for pattern separation in the 
+compression layer.
+"""
 class EntorhinalLayer(nn.Module):
     def __init__(self, query_dim, ent_dim):
         super().__init__()
@@ -92,8 +99,19 @@ class EntorhinalLayer(nn.Module):
         projection = self.nonlin(projection)
 
         return projection
+    
 
-class Familiarity(nn.Module):
+"""
+Familiarity Score
+
+Our familiary score is a learned scalar that represents how novel a memory is. If a memory is novel
+or receives a higher familiarity score, that memory will be given higher priority for replay. This
+score will be used for many purposes:
+First (Storage), this score will be used as a priority metric allowing the memory to optimize storage.
+Second (Retrieval), it serves as a threshold for retrieved memories. When we retrieve a memory, we pass it back through our familiarity layer and if the score is not beyond a threshold, we do not use it.
+
+"""
+class FamiliarityStorage(nn.Module):
     def __init__(self, query_dim):
         super().__init__()
 
@@ -102,6 +120,18 @@ class Familiarity(nn.Module):
     def forward(self, query):
         return self.lin(query)
     
+
+class FamiliarityRetrieval(nn.Module):
+    def __init__(self, query_dim, candidate_dim):
+        super().__init__()
+
+        self.score = nn.Linear(query_dim + candidate_dim, 1)
+
+    def forward(self, query, candidate):
+
+        cand = torch.cat(query, candidate)
+        score = self.score(cand)
+        return score
 
 def kWTA(input, k):
 
@@ -116,8 +146,16 @@ def kWTA(input, k):
     # Apply the mask to the original tensor
     return input * mask
 
+"""
+The Compression Layer (Dentate Gyrus/CA3)
+
+The compression layer is receives the output from the entorhinal layer and the storage retrieval score. It then signficiantly expands it into a very high dimension space that will be used to 
+sparsify for pattern separation. The ReLU begins the pattern seprartion by removing negatives.
+We significantly sparsify using K-Winner-Takes-All to finalize pattern separation. Our output should have ~2-5% activiation.
+"""
+
 class CompressionLayer(nn.Module):
-    def __init__(self, ent_dim, expansion, k=500):
+    def __init__(self, ent_dim, expansion, k=512):
         super().__init__()
 
         self.input_dim = ent_dim + 1 #Query + familiarity score
@@ -138,8 +176,17 @@ class CompressionLayer(nn.Module):
         return z_sparse
         
 
+"""
+Stage 2 Memory (Abstract Reconstruction)
+
+Our Stage 2 memory creates an abstract reconstruction of the memory we are looking to store. This
+is where our memory is contextualized and prepared for long-term storage. We replay through our stage
+2 memory during downtime for long-term consolidation. We input not only z_sparse, but also the 
+hidden state from our stage 1 memoy as this serves as a context embedding.
+"""
+
 class Stage2(nn.Module):
-    def __init__(self, z_dim, num_latents, latent_size, num_layers):
+    def __init__(self, z_dim, ctx_dim, num_latents, latent_size, num_layers):
 
         """
         Here we use a Perceiver Style Cross Attention mechanism. Our Stage 2 memory
@@ -149,7 +196,7 @@ class Stage2(nn.Module):
         """
         super().__init__()
 
-        self.input_proj = nn.Linear(z_dim, latent_size)
+        self.input_proj = nn.Linear(z_dim + ctx_dim, latent_size)
 
         self.latents = nn.Parameter(torch.randn((num_latents, latent_size)))
 
@@ -164,7 +211,7 @@ class Stage2(nn.Module):
             for _ in range(num_layers)
         ])
 
-    def forward(self, z_sparse):
+    def forward(self, z_sparse, context_embedding):
         # Expand latents for batch
         B = z_sparse.size(0)
         latents = self.latents.unsqueeze(0).expand(B, -1, -1)
@@ -177,7 +224,50 @@ class Stage2(nn.Module):
         return latents #(B, num_latents, latent_size)
 
 
+"""
+Stage 1 Memory (High-Fidelity Reconstuction)
 
+Our Stage 1 Memory is responsible for avoid catastrophic forgetting. The layer takes in just z_sparse as input, but also uses the key and value vectors from our LLM to contextualize the reconstruction. This layer's output is never actually used, but this is intentional. What we want from this layer is context. We get context in two forms: gradient and hidden state. Each memory that is stored is first passed through Stage 1 and ran through multiple times with an update until we have an accurate enough reconstruction (determined by familiarity score). These updates create a very rich hidden state that is then passed as context into our Stage 2 memory when it is time for long-term consolidation. The gradients from these updates flow to the earlier layers of the AH ensuring that the mappings and weights for this memory are persisted. Loss is computed by comparing reconstruction to original LLM embedding for that memory.
+"""
+class Stage1(nn.Module):
+    """
+    Our Stage 1 will perform cross attention with the Key and Value from the attention block from
+    the LLL and we will learn the query.
+    """
+    def __init__(self, z_dim, emb_dim, hidden_dim):
+        super().__init__()
+
+        self.input_proj = nn.Linear(z_dim, emb_dim)
+        self.cross_attn = nn.MultiheadAttention(emb_dim, num_heads=4, batch_first=True)
+        self.ln1 = nn.LayerNorm(emb_dim)
+        self.mlp = nn.Sequential(
+            nn.Linear(emb_dim, hidden_dim),
+            nn.SiLU(),
+            nn.Linear(hidden_dim, emb_dim)
+        )
+        self.ln2 = nn.LayerNorm(emb_dim)
+
+    def forward(self, z_sparse, llm_k, llm_v):
+
+        z_proj = self.input_proj(z_sparse)
+        attn_out = self.cross_attn(z_proj, llm_k, llm_v)
+        z = self.ln1(z_proj + attn_out)
+        mlp_out = self.mlp(z)
+        z_recon = self.ln2(z + mlp_out)
+
+        return z_recon
+    
+
+"""
+Integration Layer (CA1)
+
+Our Integration Layer is responsible 
+"""
+
+class IntegrationLayer(nn.Module):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
 
 if __name__ == "__main__":
     # Load tokenizer
